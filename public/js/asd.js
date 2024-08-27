ad6e9cad60571cfd5195c98d3a378ae3c453fa39
revealdone <muhamadwahidromdoni@gmail.com>
2024-08-27 15:04:32 +0700

    Pesan commit Anda di sini

--- public/js/asd.js
+++ public/js/asd.js
@@ -10,4 +10,24 @@ document.addEventListener("DOMContentLoaded", () => {
   loginLink.addEventListener("click", () => {
     wrapper.classList.remove("active");
   });
+
+  // navbar fixed
+  window.onscroll = function () {
+    const header = document.querySelector("header");
+    const fixedNav = header.offsetTop;
+
+    if (window.scrollY > fixedNav) {
+      header.classList.add("navbar-fixed");
+    } else {
+      header.classList.remove("navbar-fixed");
+    }
+  };
+
+  // hamburger
+  const hamburger = document.querySelector("#hamburger");
+  const navMenu = document.querySelector("#nav-menu");
+
+  hamburger.addEventListener("click", function () {
+    navMenu.classList.toggle("active");
+  });
 });
